d3ee1d0833ec52acd731cdecb20feed230b1b0c8
Marwen Aouiti <marwen.aouiti@outlook.fr>
2020-09-26 12:36:17 +0100

    setting socket.io & redux

--- README.md
+++ README.md
@@ -20,7 +20,6 @@ Project is in development, the list will be updated after finishing.
 To run this project, install it locally using npm:
 
 ```
-$ cd ../lorem
 $ npm install
 $ npm start
 ```
--- test/core_spec.js
+++ test/core_spec.js
@@ -50,7 +50,7 @@ describe('Application logic', () => {
         vote: Map({
           pair: List.of('Sunshine', 'Millions')
         }),
-        entries: List.of('127 Hours', 'Trainspotting')
+        entries: List.of('127 hours', 'Trainspotting')
       }));
     });
 
@@ -73,19 +73,30 @@ describe('Application logic', () => {
         entries: List.of('127 Hours', 'Trainspotting', '28 Days Later')
       }));
     });
+
+    it('marks winner when just one entry left', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 4,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        winner: 'Trainspotting'
+      }));
+    })
   });
- /**
-  * In the implementation we'll just concatenate the "winners" of the current vote to the entries. 
-  * We can find those winners with a new function called getWinners:
-  */
+
   describe('vote', () => {
 
     it('creates a tally for the voted entry', () => {
       const state = Map({
-        vote: Map({
-          pair: List.of('Trainspotting', '28 Days Later')
-        }),
-        entries: List()
+        pair: List.of('Trainspotting', '28 Days Later')
       });
       const nextState = vote(state, 'Trainspotting');
       expect(nextState).to.equal(Map({
@@ -95,31 +106,24 @@ describe('Application logic', () => {
             'Trainspotting': 1
           })
         }),
-        entries: List()
       }));
     });
 
     it('adds to existing tally for the voted entry', () => {
       const state = Map({
-        vote: Map({
-          pair: List.of('Trainspotting', '28 Days Later'),
-          tally: Map({
-            'Trainspotting': 3,
-            '28 Days Later': 2
-          })
-        }),
-        entries: List()
+        pair: List.of('Trainspotting', '28 Days Later'),
+        tally: Map({
+          'Trainspotting': 3,
+          '28 Days Later': 2
+        })
       });
       const nextState = vote(state, 'Trainspotting');
       expect(nextState).to.equal(Map({
-        vote: Map({
-          pair: List.of('Trainspotting', '28 Days Later'),
-          tally: Map({
-            'Trainspotting': 4,
-            '28 Days Later': 2
-          })
-        }),
-        entries: List()
+        pair: List.of('Trainspotting', '28 Days Later'),
+        tally: Map({
+          'Trainspotting': 4,
+          '28 Days Later': 2
+        })
       }));
     });
 
